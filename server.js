8f6a5c02dcd5f85290c535f469bc93298de06b86
zwei <jaztincontreras2805@gmail.com>
2025-11-14 18:12:15 +0800

    Updated index.html and fixes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static("public"));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "home.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // ✅ Start server
